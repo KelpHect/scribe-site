521fe3f61ce278f169aaf8d166c25dfb401526a8
Mário Silva <kellhect@gmail.com>
2026-04-09 21:01:15 +0100

    set domain to scribe.rusticstack.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://scribe.mariosilva.dev',
+  site: 'https://scribe.rusticstack.com',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
